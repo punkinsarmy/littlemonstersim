982874f0ebd47bc75483b0a5915920a937a49b02
Tom Marsh <ts.marsh@gmail.com>
2015-09-20 12:12:24 -0400

    Fixing formating

--- test/little_monster_simulation/core_test.clj
+++ test/little_monster_simulation/core_test.clj
@@ -14,17 +14,17 @@
   (testing "a monster with no stats is dead"
     (let [monster (c/monster)]
       (is (not (c/alive? monster)))))
-  
+
   (testing "a monster with life some life is alive"
     (let [monster (c/monster)
           monster' (assoc monster :life [100 identity-behaviour])]
       (is (c/alive? monster'))))
-  
+
   (testing "can get value of a stat"
     (let [monster (c/monster)
           monster' (assoc monster :life [100 identity-behaviour])]
       (is (= 100 (c/value-of monster' :life)))))
-  
+
   (testing "life can change with time"
     (let [monster (c/monster)
           monster' (assoc monster :life [100 halvit-behaviour])
