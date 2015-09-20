bc49dd14949a0caa4d3505cfb64cfef294ed77ea
Tom Marsh <ts.marsh@gmail.com>
2015-09-20 12:11:39 -0400

    Adding coverage and formatting

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-cloverage "1.0.6"]
+            [lein-cljfmt "0.1.12"]])
