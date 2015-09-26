979c43bf1813392f3b2d2097293319619885e960
Tom Marsh <ts.marsh@gmail.com>
2015-09-25 21:05:27 -0400

    Added some helper functions

--- src/little_monster_simulation/core.clj
+++ src/little_monster_simulation/core.clj
@@ -1,5 +1,31 @@
 (ns little-monster-simulation.core)
 
+(defn dissoc-in
+  "Dissociates an entry from a nested associative structure returning a new
+  nested structure. keys is a sequence of keys. Any empty maps that result
+  will not be present in the new structure."
+  [m [k & ks :as keys]]
+  (if ks
+    (if-let [nextmap (get m k)]
+      (let [newmap (dissoc-in nextmap ks)]
+        (if (seq newmap)
+          (assoc m k newmap)
+          (dissoc m k)))
+      m)
+    (dissoc m k)))
+
+(defn behaviour [monster name value]
+  (assoc-in monster [:behaviours name] value))
+
+(defn remove-behaviour [monster name]
+  (dissoc-in monster [:behaviours name]))
+
+(defn stat [monster name value]
+  (assoc-in monster [:stats name] value))
+
+(defn remove-stat [monster name]
+  (dissoc-in monster [:stats name]))
+
 (defn monster [] {:stats {} :behaviours (array-map)})
 
 (defn value-of [monster stat]
--- test/little_monster_simulation/core_test.clj
+++ test/little_monster_simulation/core_test.clj
@@ -1,5 +1,5 @@
-3(ns little-monster-simulation.core-test
-   (:require [clojure.test :refer :all]
+(ns little-monster-simulation.core-test
+  (:require [clojure.test :refer :all]
             [little-monster-simulation.core :as c]))
 
 (defn identity-behaviour
@@ -10,20 +10,6 @@
   [stat]
   (fn [monster _]
     (update-in monster [:stats stat] (partial * 0.5))))
-
-(defn dissoc-in
-  "Dissociates an entry from a nested associative structure returning a new
-  nested structure. keys is a sequence of keys. Any empty maps that result
-  will not be present in the new structure."
-  [m [k & ks :as keys]]
-  (if ks
-    (if-let [nextmap (get m k)]
-      (let [newmap (dissoc-in nextmap ks)]
-        (if (seq newmap)
-          (assoc m k newmap)
-          (dissoc m k)))
-      m)
-    (dissoc m k)))
 
 (deftest monsters
   (testing "a monster with no stats is dead"
@@ -37,7 +23,7 @@
   (testing "a monster with 0 life is dead"
     (let [monster {:stats {:life 0}}]
       (is (not (c/alive? monster)))))
-    
+
   (testing "can get value of a stat"
     (let [monster {:stats {:life 100} :behaviours {}}]
       (is (= 100 (c/value-of monster :life)))))
@@ -51,30 +37,29 @@
   (testing "behaviours can expire"
     (let [monster {:stats {:life 100}
                    :behaviours {:one-shot (fn [m _]
-                                            (dissoc-in m [:behaviours :one-shot]))}}
+                                            (c/remove-behaviour m :one-shot))}}
           monster' (c/tick monster 1)]
       (is (empty? (:behaviours monster')))))
-  
+
   (testing "behaviours can add stats"
     (let [monster {:stats {:life 100}
                    :behaviours {:walkies (fn [m _]
-                                           (assoc-in m [:stats :walk] 100))}}
+                                           (c/stat m :walk 100))}}
           monster' (c/tick monster 1)]
       (is (= 100 (c/value-of monster' :walk)))))
-  
+
   (testing "behaviours can add other behaviours"
     (let [monster {:stats {:life 100}
                    :behaviours {:add-walk (fn [m _]
-                                            (assoc-in m
-                                                      [:behaviours :walk]
-                                                      (fn [m _]
-                                                        (assoc-in m [:stats :walk] 100))))}}
+                                            (c/behaviour m :walk 
+                                                         (fn [m _]
+                                                           (c/stat m :walk 100))))}}
           monster' (c/tick (c/tick monster 1) 1)]
       (is (= 100 (c/value-of monster' :walk)))))
 
   (testing "behaviours can remove stats"
     (let [monster {:stats {:life 100 :foo 5}
                    :behaviours {:kill-foo (fn [m _]
-                                             (dissoc-in m [:stats :foo]))}}
+                                            (c/remove-stat m :foo))}}
           monster' (c/tick monster 1)]
       (is (nil? ((comp :foo :stats) monster'))))))
